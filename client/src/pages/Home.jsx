3e8b1f126b4d76d0dac6ceb32de1de87fb9f69a4
Kiatolao <engramma.site@gmail.com>
2024-02-17 08:35:57 +0100

    fixed vh on mobile

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,7 +50,7 @@ export const Home = () => {
 
   return (
     <>
-    <div style={divStyle} className="p-5 bg-no-repeat bg-center bg-cover h-screen shadow-xl flex bg-black bg-opacity-40 justify-center items-center">
+    <div style={divStyle} className="p-5 bg-no-repeat bg-center bg-cover h-[95vh] md:h-screen shadow-xl flex bg-black bg-opacity-40 justify-center items-center">
 
       <div className="absolute inset-0 flex flex-col justify-center items-center ">
       <h1 className="text-5xl md:text-7xl" style={{ opacity }}>
